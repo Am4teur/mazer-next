ebcf9be719199159556c4ad166b89b007223ba3f
Daniel Castro <daniel.castro@digitalhub.man>
2022-03-27 21:43:25 +0100

    Add correct color and animations to Button component

--- components/basics/Button.tsx
+++ components/basics/Button.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 interface IButtonProps {
   children: React.ReactNode;
@@ -6,9 +7,13 @@ interface IButtonProps {
 
 const Button = ({ children }: IButtonProps) => {
   return (
-    <button className="border-solid border-2 border-sky-800 bg-sky-500 rounded-lg px-4 my-2">
+    <motion.button
+      className="border-solid border-2 bg-blue-3 rounded-lg px-4 my-2"
+      whileTap={{ y: 2 }}
+      whileHover={{ scale: 1.1 }}
+    >
       {children}
-    </button>
+    </motion.button>
   );
 };
 
