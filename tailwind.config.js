e76a08e972e95225d66162db28cb47e85ce4cc0d
Daniel Castro <daniel.castro@digitalhub.man>
2022-04-08 14:52:36 +0100

    Add auth background SVG to TW theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mode: 'jit',
+  mode: "jit",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
@@ -7,16 +7,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'gray-light': '#36393f',
-        'gray-md': '#2f3136',
-        'gray-dark': '#202225',
-        'gray-whiteish': '#b9bbbe',
-        'blue-1': '#218ed8',
-        'blue-2': '#28a0f0',
-        'blue-3': '#008ff0',
-        'bg-blue': '#D1EDFF',
-      }
-    }
+        "gray-light": "#36393f",
+        "gray-md": "#2f3136",
+        "gray-dark": "#202225",
+        "gray-whiteish": "#b9bbbe",
+        "blue-1": "#218ed8",
+        "blue-2": "#28a0f0",
+        "blue-3": "#008ff0",
+        "bg-blue": "#D1EDFF",
+      },
+      backgroundImage: {
+        auth: "url('/blob-scene-haikei.svg')",
+      },
+    },
   },
   plugins: [],
-}
+};
