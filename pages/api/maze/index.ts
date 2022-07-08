5798b4edfeafbbfe817b2a2bd885d250b26bb777
Daniel Castro <daniel.castro@digitalhub.man>
2022-07-08 19:46:28 +0100

    Add get mazes API => get all mazes

---
+++ pages/api/maze/index.ts
@@ -0,0 +1,31 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import dbConnect from "@/lib/dbConnect";
+import Maze, { IMaze } from "@/models/Maze";
+
+interface MazeData {
+  mazes: IMaze[] | null;
+  error?: string;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<MazeData>
+) {
+  await dbConnect();
+
+  const { method } = req;
+
+  switch (method) {
+    case "GET":
+      try {
+        const mazes = await Maze.find();
+        return res.status(200).json({ mazes: mazes });
+      } catch (error: any) {
+        return res.status(400).json({ mazes: null, error: error.toString() });
+      }
+    default:
+      return res
+        .status(400)
+        .json({ mazes: null, error: "!method type not allowed" });
+  }
+}
