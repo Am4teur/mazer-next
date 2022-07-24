b33a6d7d8c8419646df8be619245601acad6c33e
Daniel Castro <daniel.castro@digitalhub.man>
2022-07-24 14:41:11 +0100

    Fix reload secure page (/play) does not prompt signIn

--- pages/play.tsx
+++ pages/play.tsx
@@ -2,23 +2,31 @@ import { useEffect } from "react";
 import NextLink from "next/link";
 import { useSession, signIn } from "next-auth/react";
 import Playground from "@/components/Playground";
+import Router from "next/router";
+// import { GetStaticProps, GetStaticPaths, GetServerSideProps } from "next";
+// import unstable_getServerSession from "next-auth/next";
+// import authOptions from "./api/auth/[...nextauth]";
 
 const Play = () => {
   const { data: session, status } = useSession();
-  const loading = status === "loading";
+  const isLoading = status === "loading";
 
-  useEffect(() => {
-    const securePage = async () => {
-      if (!session) {
-        signIn();
-      }
-    };
+  // not very happy with this, because I would like to have a solution like the one
+  // on the Nextauth doc page (things that are commented)
+  // https://next-auth.js.org/tutorials/securing-pages-and-api-routes
+  const redirectToSignIn = () => {
+    const { pathname } = Router;
+    if (pathname === "/play") {
+      Router.push("/auth");
+    }
+  };
 
-    securePage();
-  }, [session]);
-
-  if (loading) {
+  if (isLoading) {
     return <h1>Loading...</h1>;
+  }
+
+  if (!session) {
+    redirectToSignIn();
   }
 
   return (
@@ -31,5 +39,17 @@ const Play = () => {
     </>
   );
 };
+
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   return {
+//     props: {
+//       session: await unstable_getServerSession(
+//         context.req, // why is this giving an error
+//         context.res,
+//         authOptions
+//       ),
+//     },
+//   };
+// };
 
 export default Play;
