8db37027f88b12dfdd8ab6e1366f114e1c2a9024
Daniel Castro <daniel.castro@digitalhub.man>
2022-09-29 19:41:21 +0100

    Add font rubik and colors to theme
    Design changes

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,4 +1,5 @@
 import { extendTheme } from "@chakra-ui/react";
+import "@fontsource/rubik";
 
 const theme = extendTheme({
   fonts: {
@@ -16,7 +17,13 @@ const theme = extendTheme({
       "blue-1": "#218ed8",
       "blue-2": "#28a0f0",
       "blue-3": "#008ff0",
+      // hover is the brand.blue-3 from 47% to 70%
+      // https://www.w3schools.com/colors/colors_converter.asp
+      "blue-3-hover": "hsl(204, 100%, 70%)",
       "bg-blue": "#D1EDFF",
+      "blue-4": "#105093",
+      "blue-5": "#166cc6",
+      "blue-6": "#0a3460",
     },
   },
 });
